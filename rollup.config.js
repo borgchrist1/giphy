93b593b3bf9dc00e5728995f19eb5abf451e6e82
borgchrist1 <christian.borg@outlook.com>
2017-02-06 11:55:17 +0100

    more pacages

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/scripts/index.js',
