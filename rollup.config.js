5f2f2c179a50b1374929f3ee3ade5dda184343c9
borgchrist1 <christian.borg@outlook.com>
2017-02-06 10:16:13 +0100

    bla

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,19 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/scripts/index.js',
   dest: 'public/assets/scripts/giphy.js',
   format: 'iife',
-  sourceMap: true
+  sourceMap: true,
+  "plugins":[
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonjs(),
+    resolve({
+      jsnext: true,
+      browser: true
+    }),
+    (process.env.NODE_ENV === 'production' && uglify())
+  ]
 };
